8944a2dd04173e51f813ff04f5006a70ced5fdd7
karuppasamy-avani <karuppasamy.avani@aspiresys.com>
2025-03-12 19:50:38 +0530

    created transaction service

---
+++ transaction-service/src/main/java/com/bankofindia/transaction_service/model/TransactionStatus.java
@@ -0,0 +1,5 @@
+package com.bankofindia.transaction_service.model;
+
+public enum TransactionStatus {
+	SUCCESS, FAILED, PENDING
+}
---
+++ transaction-service/src/main/java/com/bankofindia/transaction_service/model/entity/Transaction.java
@@ -0,0 +1,43 @@
+package com.bankofindia.transaction_service.model.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.bankofindia.transaction_service.model.TransactionStatus;
+import com.bankofindia.transaction_service.model.TransactionType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;  
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime transactionTime;
+}
